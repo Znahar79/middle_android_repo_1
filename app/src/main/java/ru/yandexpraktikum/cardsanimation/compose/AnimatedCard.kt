4e601c699964722110704e6c4f722fea9ab6b49b
Znahar79 <znakhar1998@yandex.ru>
2025-12-03 21:15:59 +0300

    Sprint1: Fixes by review

--- app/src/main/java/ru/yandexpraktikum/cardsanimation/compose/AnimatedCard.kt
+++ app/src/main/java/ru/yandexpraktikum/cardsanimation/compose/AnimatedCard.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import ru.yandexpraktikum.cardsanimation.ANIMATION_DURATION_LONG
+import ru.yandexpraktikum.cardsanimation.ANIMATION_DURATION_SHORT
 import ru.yandexpraktikum.cardsanimation.model.CardData
 import kotlin.math.cos
 import kotlin.math.sin
@@ -28,41 +30,42 @@ fun AnimatedCard(
     cardData: CardData,
     targetRotation: Float,
     animationState: CardSwapAnimationState,
-    onAnimationStepComplete: ((Int) -> Unit)
+    onAnimationStepComplete: ((AnimationPhase) -> Unit)
 ) {
     val rotationAnimation by animateFloatAsState(
         targetValue = when {
-            animationState.animationStep == 3 -> targetRotation
+            animationState.animationStep == AnimationPhase.PHASE_THREE -> targetRotation
             animationState.isAnimating -> targetRotation
             else -> targetRotation
         },
-        animationSpec = tween(durationMillis = if (animationState.animationStep == 3) 300 else 800),
+        animationSpec = tween(durationMillis = if (animationState.animationStep == AnimationPhase.PHASE_THREE) ANIMATION_DURATION_SHORT else ANIMATION_DURATION_LONG),
         finishedListener = {
-            if (animationState.animationStep == 3 && animationState.isAnimating) onAnimationStepComplete.invoke(3)
+            if (animationState.animationStep == AnimationPhase.PHASE_THREE && animationState.isAnimating) onAnimationStepComplete.invoke(AnimationPhase.PHASE_THREE)
         },
         label = "rotation"
     )
     val density = LocalDensity.current
 
-    val shouldBringToFront = animationState.isAnimating && animationState.animationStep >= 2
+    val shouldBringToFront = animationState.isAnimating && animationState.animationStep >= AnimationPhase.PHASE_TWO
 
     val animatedTranslationX by animateFloatAsState(
         targetValue = when {
-            animationState.isAnimating &&  animationState.animationStep == 1 -> {
+            animationState.isAnimating && animationState.animationStep == AnimationPhase.PHASE_ONE -> {
                 val moveDistance = with(density) { 50.dp.toPx() }
                 val rotationRad = Math.toRadians(targetRotation.toDouble())
                 moveDistance * cos(rotationRad).toFloat()
             }
-            animationState.isAnimating && animationState.animationStep == 2 -> 0f // Движение в центр
+            animationState.isAnimating && animationState.animationStep == AnimationPhase.PHASE_TWO -> 0f // Движение в центр
             else -> 0f
         },
         animationSpec = tween(durationMillis = 300),
         finishedListener = {
             if (animationState.isAnimating) {
                 when (animationState.animationStep) {
-                    1 -> onAnimationStepComplete.invoke(1)
-                    2 -> onAnimationStepComplete.invoke(2)
-                    3 ->  onAnimationStepComplete.invoke(3)
+                    AnimationPhase.PHASE_ONE -> onAnimationStepComplete.invoke(AnimationPhase.PHASE_ONE)
+                    AnimationPhase.PHASE_TWO -> onAnimationStepComplete.invoke(AnimationPhase.PHASE_TWO)
+                    AnimationPhase.PHASE_THREE -> onAnimationStepComplete.invoke(AnimationPhase.PHASE_THREE)
+                    else -> {}
                 }
             }
         },
@@ -71,12 +74,12 @@ fun AnimatedCard(
 
     val animatedTranslationY by animateFloatAsState(
         targetValue = when {
-            animationState.isAnimating && animationState.animationStep == 1 -> {
+            animationState.isAnimating && animationState.animationStep == AnimationPhase.PHASE_ONE -> {
                 val moveDistance = with(density) { 50.dp.toPx() }
                 val rotationRad = Math.toRadians(targetRotation.toDouble())
                 moveDistance * sin(rotationRad).toFloat()
             }
-            animationState.isAnimating && animationState.animationStep == 2 -> 0f // Движение в центр
+            animationState.isAnimating && animationState.animationStep == AnimationPhase.PHASE_TWO -> 0f // Движение в центр
             else -> 0f
         },
         animationSpec = tween(durationMillis = 300),
